d77c29e22fdb4bd8b7aa9cbf524327e53b63019b
leospurs <lehojae@gmail.com>
2021-10-19 12:33:58 +0900

    강의

---
+++ Project0/src/chapter08/PointTwo.java
@@ -0,0 +1,17 @@
+package chapter08;
+
+public class PointTwo {
+	
+	private int xPos;
+	private int yPos;
+	
+	PointTwo(int x, int y) {
+		xPos = x;
+		yPos = y;
+	}
+
+	@Override
+	public String toString() {
+		return "PointTwo [xPos=" + xPos + ", yPos=" + yPos + "]";
+	}
+}
--- Project0/src/chapther07/FriendInfoMain.java
+++ Project0/src/chapther07/FriendInfoMain.java
@@ -1,47 +1,46 @@
 package chapther07;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class FriendInfoMain {
-	
-public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		FriendInfoHandler handler = new FriendInfoHandler();
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
-			
+
+		while (true) {
+
 			System.out.println("##### 메뉴 선택 #####");
-			System.out.println(" 1. 고교 친구 정보 저장");
-			System.out.println(" 2. 대학 친구 정보 저장");
-			System.out.println(" 3. 전체 정보 출력");
-			System.out.println(" 4. 기본 정보 출력");
-			System.out.println(" 5. 프로그램 종료");
+			System.out.println(Menu.INSERT_HI + ". 고교 친구 정보 저장");
+			System.out.println(Menu.INSERT_UNIV + ". 대학 친구 정보 저장");
+			System.out.println(Menu.SHOW_ALL + ". 전체 정보 출력");
+			System.out.println(Menu.SHOW_BASIC + ". 기본 정보 출력");
+			System.out.println(Menu.EXIT + ". 프로그램 종료");
 			System.out.println("메뉴 선택 >>");
-			
+
 			int select = Integer.parseInt(sc.nextLine());
-			
-			switch(select) {
-			case 1: case 2:
+
+			switch (select) {
+			case Menu.INSERT_HI:
+			case Menu.INSERT_UNIV:
 				handler.addFriend(select);
 				break;
-			case 3:
+			case Menu.SHOW_ALL:
 				handler.showAllData();
 				break;
-			case 4:
+			case Menu.SHOW_BASIC:
 				handler.ShowAllSimpleData();
 				break;
-			case 5:
+			case Menu.EXIT:
 				System.out.println("프로그램을 종료합니다.");
 				System.exit(0); // return
 			}
-			
-			
-			
+
 		}
-		
 
 	}
 
-}	
+}
