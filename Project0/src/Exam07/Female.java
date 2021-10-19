488e686e3e5d03727a54e570296395ef538c645b
leospurs <lehojae@gmail.com>
2021-10-19 17:54:43 +0900

    강의

---
+++ Project0/src/Exam07/Female.java
@@ -0,0 +1,31 @@
+package Exam07;
+
+public class Female extends Person {
+
+//	2.Person 클래스를 상속해서 확장하는 새로운 클래스 Male 클래스와 Female 클래스를 정의 해봅시다.
+//
+//	①각 클래스는 상속 받은 멤버 외에 추가적인 변수와 메소드를 추가해서 새로운 클래스를 정의해봅시다.
+//
+//	②각 클래스는 상속 받은 멤버 외에 추가적인 변수와 메소드를 추가해서 새로운 클래스를 정의해봅시다.
+//
+//	③Person 클래스에서 정의된 인사하는 메소드를 오버라이딩 해봅시다.
+//
+//	④Person 클래스에 생성자를 정의해서 인스턴스 변수들을 초기화 해봅시다.
+
+	String name;
+	String juminNum;
+	String gender;
+	
+	public Female(String name, String juminNum) {
+		super(name, juminNum);
+		this.gender = "여성";
+	}
+	
+	
+	@Override
+	public void sayHello() {
+		super.sayHello();
+		System.out.println("성별은 " + this.gender + "입니다.");
+	}
+
+}
---
+++ Project0/src/Exam07/Person.java
@@ -0,0 +1,50 @@
+package Exam07;
+
+import java.util.Calendar;
+
+public class Person {
+
+	// 이름을 저장하는 변수, 주민등록번호를 저장하는 변수를 정의해봅시다.
+	String name; 		// 이름
+	String juminNum; 	// 주민번호(14자리, - 제외)
+	final static int THIS_YEAR = Calendar.getInstance().get(Calendar.YEAR);
+	
+	
+	// 생성자 정의
+	public Person(String name, String juminNum) {
+		this.name = name;
+		this.juminNum = juminNum;
+		
+	}
+
+	// 인사하는 메소드를 정의해봅시다.
+	// - “안녕하세요. 저는 OOO입니다. 00살 입니다.”라는 문자열이 출력하도록 정의합시다.
+	public void sayHello() {
+
+		System.out.println("안녕하세요 저는 " + name + "입니다. " + ageCal() + " 살 입니다.");
+	}
+
+	
+	// 나이 계산 메소드
+	public int ageCal() {
+		 	
+		int birthYear = Integer.parseInt(juminNum.substring(0,2)); // 주민번호 처음 두자리 수
+		int num = Integer.parseInt(juminNum.substring(7,8));
+		
+		
+		if(num == 1 || num == 2) { // 1,2 는 1900년대 생
+			
+			return THIS_YEAR - ((1900 + birthYear) + 1);
+		} else if(num == 3 || num == 4) {
+			
+			return THIS_YEAR - ((2000 + birthYear) + 1);
+		} else {
+			
+			return -1;	
+		}
+		
+		
+	}
+	
+	
+}
