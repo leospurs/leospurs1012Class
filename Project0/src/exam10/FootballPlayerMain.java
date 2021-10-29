0c7905bd973481f8d5314c6bb1f9ea553f960cc0
leospurs <lehojae@gmail.com>
2021-10-29 09:30:38 +0900

    강의 내용

--- Project0/src/exam10/FootballPlayerMain.java
+++ Project0/src/exam10/FootballPlayerMain.java
@@ -1,23 +1,24 @@
 package exam10;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
-public class LinkedListFP {
+public class FootballPlayerMain {
 
 	public static void main(String[] args) {
-		
-		
-		LinkedList<FootballPlayer> list = new LinkedList<FootballPlayer>();
-		
+
+		List<FootballPlayer> list = new ArrayList<FootballPlayer>();
+
 		list.add(new FootballPlayer("손흥민", 7, "토트넘", 29));
 		list.add(new FootballPlayer("황희찬", 26, "울버햄튼", 26));
 		list.add(new FootballPlayer("이강인", 7, "마요르카", 21));
 		list.add(new FootballPlayer("황의조", 13, "보르도", 29));
 		list.add(new FootballPlayer("정우영", 11, "프라이부르크", 21));
-		
-		for(int i = 0; i < list.size(); i++) {
-			
+
+		for (int i = 0; i < list.size(); i++) {
+
 			System.out.println(list.get(i));
+
 		}
 	}
 
