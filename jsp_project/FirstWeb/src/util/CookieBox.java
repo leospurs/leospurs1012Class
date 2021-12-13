82c3ce0a94aeeb4931c77bc73583e8b8e13f3352
leospurs <lehojae@gmail.com>
2021-12-13 18:18:01 +0900

    Cookie 관리

--- jsp_project/FirstWeb/src/util/CookieBox.java
+++ jsp_project/FirstWeb/src/util/CookieBox.java
@@ -1,9 +1,13 @@
 package util;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 // 클래스의 용도 : 현재 사용하는 시점의 요청에 있는 쿠기 값들을 저장하고 관리 기능을 제공하는 크래스
 // 쿠키생성, 쿠키 제공, 쿠키 값 제공, 쿠키 존재 유무 확인
@@ -14,7 +18,67 @@ public class CookieBox {
 	private Map<String, Cookie> cookieMap = new HashMap<String, Cookie>();
 	
 	// 생성자를 통해서 값을 초기화 : Map에 Cookie 객체들을 저장
-	public CookieBox() {
+	public CookieBox(HttpServletRequest request) {
+		
+		Cookie[] cookies = request.getCookies();
+		
+		if(cookies != null) {
+			for(int i=0; i<cookies.length; i++) {
+				cookieMap.put(cookies[i].getName(), cookies[i]);
+			}
+		}
 		
 	}
+
+	
+	// 쿠키 생성 메소드 : 인스턴스 생성 없이 사용가능하도록 한다. -> static
+	// 쿠키 생성시 필요한 데이터 : 쿠키 이름, 쿠키 값, 유효시간, 경로, 도메인
+	// 오버로딩으로 같은 이름의 메소드를 정의
+	public static Cookie createCookie(String name, String value) throws UnsupportedEncodingException {
+		return new Cookie(name, URLEncoder.encode(value, "utf-8"));
+	}
+
+	public static Cookie createCookie(String name, String value, int maxAge) throws UnsupportedEncodingException {
+		Cookie c = new Cookie(name, URLEncoder.encode(value, "utf-8"));
+		c.setMaxAge(maxAge);
+		return c;
+	}
+
+	public static Cookie createCookie(String name, String value, int maxAge, String path) throws UnsupportedEncodingException {
+		Cookie c = new Cookie(name, URLEncoder.encode(value, "utf-8"));
+		c.setMaxAge(maxAge);
+		c.setPath(path);
+		return c;
+	}
+
+	public static Cookie createCookie(String name, String value, int maxAge, String path, String domain) throws UnsupportedEncodingException {
+		Cookie c = new Cookie(name, URLEncoder.encode(value, "utf-8"));
+		c.setMaxAge(maxAge);
+		c.setPath(path);
+		c.setDomain(domain);
+		return c;
+	}
+	
+	// 쿠키 객체를 제공하는 메소드
+		public Cookie getCookie(String cookieName) {
+			return cookieMap.get(cookieName);
+		}
+		
+		// 쿠키 저장값을 반환 : String
+		public String getValue(String cookieName) throws UnsupportedEncodingException {
+			
+			Cookie c = cookieMap.get(cookieName);
+			
+			if(c == null) {
+				return null;
+			}
+			
+			return URLDecoder.decode(c.getValue(), "utf-8") ;
+		}
+		
+		
+		// 쿠키 존재 유무 확인 : 존재 하면 true, false
+		public boolean exists(String cookieName) {
+			return cookieMap.get(cookieName) != null;
+		}
 }
