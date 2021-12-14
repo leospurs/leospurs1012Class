48cbcd70dcd4cec30faecbeee5a9ed333e2ac911
leospurs <lehojae@gmail.com>
2021-12-15 00:16:58 +0900

    MVC

--- jsp_project/MVC/src/controller/FrontController.java
+++ jsp_project/MVC/src/controller/FrontController.java
@@ -1,15 +1,105 @@
 package controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.Command;
+
 public class FrontController extends HttpServlet{
+
+	private Map<String, Command> commands = new HashMap<String, Command>();
+	
+	
+	
+	
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+		System.out.println("서블릿이 생성될때 한번 실행합니다 -> 초기화 처리 합니다!!!");
+		
+		// 설정 파일에 init-param 설정된 경로 가져오기
+		String configFile = config.getInitParameter("configPath");
+		// 시스템 경로 실제 경로
+		String configPath = config.getServletContext().getRealPath(configFile);
+		
+				
+		// 프로퍼티(MAP 파생된 클래스) 파일 생성
+		// 파일 -> 객체
+		
+		Properties prop = new Properties();
+		FileInputStream fis = null;
+		
+		try {
+			
+			fis = new FileInputStream(configPath);
+			
+			// 파일 -> 객체 Properties
+			prop.load(fis);
+			
+			fis.close();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		Iterator<Object> itr = prop.keySet().iterator();
+		
+		while(itr.hasNext()) {
+			
+			String command = (String) itr.next();
+			String commandClassName = prop.getProperty(command);
+			
+			System.out.println(command+"="+commandClassName);
+			
+			// commands Map 에 command(key), commandClassName의 인스턴스를 저장
+			
+			// commandClassName 이름의 클래스로 인스턴스 생성 -> Class 클래스를 이용
+			
+			try {
+				// 클래스 이름(full name)으로 인스턴스 생성
+				Class commandClass = Class.forName(commandClassName);
+				
+				// 인스턴스 생성
+				Command commandObj = (Command)commandClass.newInstance();
+				
+				// commands Map에 <요청, 객체> 저장
+				commands.put(command, commandObj);
+				
+				
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+			// Command 설정
+			//commands.put("/date.do", new DateCommandImpl());
+			//commands.put("/greeting.do", new GreetingCommandImpl());
+				
+			System.out.println("초기화된 요청 : " + commands.size());
+		
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
